b16e2648262d846bba3105d969f7e570bfa54a0f
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-08-22 15:57:56 -0400

    victorspasswordgenerator

---
+++ script.js
@@ -0,0 +1,32 @@
+// Assignment code here
+function generatePassword() {
+// in order to generate password, must include special characters"
+var specialcharacters= [' ', '!', '"', '#', '$', '%', '&', "'",'(', ')', '*', '+',',', '-', '.', '/', ':', ';', '<', '=', '<', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '"' ] ;
+
+// in order to generate password, must include lowercase letter"
+var lowercaseletters= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// in order to generate password, must include uppercase letters"
+var uppercaseletters= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+// in order to generate password, must include numbers"
+var numbers= ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+//
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
