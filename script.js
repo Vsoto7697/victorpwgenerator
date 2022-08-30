ab2a0913976b91f949e37b4b1b1c5fbe09e5f1c7
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-08-29 21:07:18 -0400

    added a series of prompts to determine user's ideal password

--- script.js
+++ script.js
@@ -12,9 +12,27 @@ var uppercaseletters= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L
 // in order to generate password, must include numbers"
 var numbers= ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-//
+console.log(specialcharacters,lowercaseletters,uppercaseletters,numbers)
+
+var generateBtn = document.querySelector("generate")
+while (true) {
+// give user the option on how many characters they want in their password"
+var userinput= window.prompt("Hello... How long do you want your password to be?")
+// return all null responses so that prompts don't keep popping up"
+if (userinput === null) {
+return
+}
+var paswordLength =parseInt (userinput)
+
+// break all invalid password lengths"
+if (isNaN(passwordLength)) {
+window.alert("Please try againn")
+}
 
 }
+}
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
