fc7a010a5e61851e205e2cd3f0b75a0b31a5beda
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-09-04 13:05:32 -0400

    added variables to acknowledge user input for password criteria

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // Assignment code here
 var generateBtn = document.querySelector("generate");
 
+//creating a password criteria
+var PasswordCriteria = {
+
+  PasswordLength: 0,
+
+  // array for lowercase letters
+  LowerCaseletters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  
+  // array for uppercase letters
+  UpperCaseletters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+  //array for numbers
+  Numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  //array for symbols
+  Symbols: ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",",
+    "-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
