d2ef83d324b3f77b55d1344496306a17ad003680
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-09-04 11:39:35 -0400

    fixed era when trying to generate password

--- script.js
+++ script.js
@@ -1,81 +1,87 @@
 // Assignment code here
 var generateBtn = document.querySelector("generate");
+function getRandom(arr) {
+  var randomIdx = Math.floor(Math.random() * arr.length)
+  var randomElement = arr(randomIdx)
+  return
+}
+// function getRandomitem(list) {
+//   console.log(specialcharacters, "list")
+//   return list[randomInt(specialcharacters, lowercaseletters, uppercaseletters, numbers)]
+// }
 
-function randomInt(min, max) {
-if (!max) {
-max=min
-min=0
-}
-var rand = Math.random()
-return Math.floor(min*(1 - rand) +rand*max)
-}
-function getRandomitem(list) {
-return list [randomInt(list.length)]
-}
-
+ // give user the option to include either special characters, numbers, uppercase, and lower case letters"
+ var userwantsspecialcharacters = window.confirm("Do you want to include symbols in your password?")
+ var userwantslowercaseletters = window.confirm("Do you want to include lowercase letters in your password?")
+ var userwantsuppercaseletters = window.confirm("Do you want to include uppercase letters in your password?")
+ var userwantsnumbers = window.confirm("Do you want to include numbers in your password?")
+ 
 function generatePassword() {
-// in order to generate password, must include special characters"
-var specialcharacters= [' ', '!', '"', '#', '$', '%', '&', "'",'(', ')', '*', '+',',', '-', '.', '/', ':', ';', '<', '=', '<', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '"' ] ;
+  // in order to generate password, must include special characters"
+  var specialcharacters = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '<', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~', '"'];
 
-// in order to generate password, must include lowercase letter"
-var lowercaseletters= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  // in order to generate password, must include lowercase letter"
+  var lowercaseletters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// in order to generate password, must include uppercase letters"
-var uppercaseletters= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  // in order to generate password, must include uppercase letters"
+  var uppercaseletters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-// in order to generate password, must include numbers"
-var numbers= ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  // in order to generate password, must include numbers"
+  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-var optionscart = []
+  var optionscart = []
 
-// if statement to have special characters in user's password
-if (userwantsspecialcharacters === true) {
-  optionscart.push(specialcharacters)
+  // if statement to have special characters in user's password
+  if (userwantsspecialcharacters === true) {
+    optionscart.push(specialcharacters)
+    console.log(specialcharacters.join(""))
+  }
+
+  // if statement to have lowercase letters in user's password
+  if (userwantslowercaseletters === true) {
+    optionscart.push(lowercaseletters)
+  }
+
+  // if statement to have uppercase letters in user's password
+  if (userwantsuppercaseletters === true) {
+    optionscart.push(uppercaseletters)
+  }
+
+  // if statement to have numbers in user's password
+  if (userwantsnumbers === true) {
+    optionscart.push(numbers)
+  }
+  console.log(optionscart.join( "" ))
+  // give user the option on how many characters they want in their password"
+  var userinput = window.prompt("Hello... How long do you want your password to be?")
+  // return all null responses so that prompts don't keep popping up"
+
+  var passwordLength = parseInt(userinput)
+
+  // break all invalid password lengths"
+  if (isNaN(passwordLength)) {
+    window.alert("Please try again, the input you entered is not a number!")
+  }
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Please try again, the password length you entered must be between 8 and 128 characters")
+  }
+
+  var generatedPassword = ""
+  console.log(optionscart)
+
+  for (var i = 0; i <= passwordLength; i++) {
+    // var randomList = getRandomitem(optionscart)
+    // console.log(randomList, "random list")
+    // var randomCharacter = getRandomitem(randomList)
+    // console.log(randomCharacter, "randomCharacter")
+    var randomCharacter = optionscart[Math.floor(Math.random() * passwordLength)]
+    console.log(randomCharacter)
+    generatedPassword += randomCharacter
+    // console.log(generatedPassword)
+  }
+  return generatedPassword
 }
-
-// if statement to have lowercase letters in user's password
-if (userwantslowercaseletters === true) {
-  optionscart.push(lowercaseletters)
-}
-
-// if statement to have uppercase letters in user's password
-if (userwantsuppercaseletters === true) {
-  optionscart.push(uppercaseletters)
-}
-
-// if statement to have numbers in user's password
-if (userwantsnumbers === true) {
-  optionscart.push(numbers)
-}
-
-while (true) {
-// give user the option on how many characters they want in their password"
-var userinput= window.prompt("Hello... How long do you want your password to be?")
-// return all null responses so that prompts don't keep popping up"
-if (userinput === null) {
-return
-}
-var passwordLength =parseInt (userinput)
-
-// break all invalid password lengths"
-if (isNaN(passwordLength)) {
-window.alert("Please try again, the input you entered is not a number!")
-} else if (passwordLength < 8 || passwordLength > 128) {
-window.alert ("Please try again, the password length you entered must be between 8 and 128 characters")
-} else {
-break
-}
-}
-
-// give user the option to include either special characters, numbers, uppercase, and lower case letters"
-var userwantsspecialcharacters = window.confirm ("Do you want to include symbols in your password?")
-var userwantslowercaseletters = window.confirm("Do you want to include lowercase letters in your password?")
-var userwantsuppercaseletters = window.confirm("Do you want to include uppercase letters in your password?")
-var userwantsnumbers = window.confirm("Do you want to include numbers in your password?")
-}
-
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -91,3 +97,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
