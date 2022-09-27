f8595829bfbf780460ca41b83c366b2efb380e9c
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2022-09-27 19:41:02 -0400

    added variables for password requirnments

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
 // Assignment code here
-var generateBtn = document.querySelector("generate");
-
-//creating a password criteria
-var PasswordCriteria = {
-
-  PasswordLength: 0,
-
-  // array for lowercase letters
-  LowerCaseletters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  
-  // array for uppercase letters
-  UpperCaseletters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-  //array for numbers
-  Numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-  //array for symbols
-  Symbols: ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",",
-    "-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]
+function generatePassword() {
+  // these variables reference password requirments 
+  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var lowercaseletters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var uppercaseletters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+  var possibleCharacters = [];
+  return finalPassword;
 }
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -35,4 +23,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
